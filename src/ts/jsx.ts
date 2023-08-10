534f0a1a251e67ceaceb32a84ee44586bb040f0d
KonkenBonken <konrad05pettersson@gmail.com>
2023-08-10 23:48:50 +0200

    add style prop support

--- src/ts/jsx.ts
+++ src/ts/jsx.ts
@@ -3,7 +3,7 @@ export default {
     name: TagName,
     props: { [id: string]: string | boolean } | null,
     ...content: (string | HTMLElementTagNameMap[TagName])[]
-  ) {
+  ): Element {
     props ||= {};
 
     const element = document.createElement(name)
@@ -11,6 +11,10 @@ export default {
     for (const [key, value] of Object.entries(props))
       if (typeof value === 'boolean')
         element.toggleAttribute(key, value);
+      else if (key === "style")
+        for (const [prop, val] of Object.entries(value))
+          // @ts-expect-error
+          element.style[prop] = val;
       else if (key === "className")
         element.classList.add(...value.split(' '));
       else
