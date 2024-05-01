76e4c389e0248743d4a60f021ae6dbe40fc0855c
KonkenBonken <konrad05pettersson@gmail.com>
2024-05-01 16:57:08 +0200

    add tile types

--- script.mjs
+++ script.mjs
@@ -4,7 +4,31 @@ class Tile {
   constructor(x, y) {
     this.x = x;
     this.y = y;
+    this.faces = [0, 0, 0, 0]; // 0:no, 1:in, 2:out
+  }
+
+  setType(type) {
+    this.type = type;
+    switch (type) {
+      case 'generator':
+        this.faces = [0, 2, 0, 0];
+      case 'belt':
+        this.faces = [0, 2, 0, 1];
+    }
+  }
+
   draw(size) {
+    switch (this.type) {
+      case 'generator':
+        fill(100);
+        break
+      case 'belt':
+        fill(150);
+        break
+      default:
+        noFill();
+    }
+
     square(
       this.x * size,
       this.y * size,
@@ -21,6 +45,10 @@ function setup() {
 
   gridSize = createVector(10, 10);
   grid = Array(gridSize.y).fill().map((_, y) => Array(gridSize.x).fill().map((_, x) => new Tile(x, y)));
+
+  grid[1][1].setType('generator');
+  grid[1][2].setType('belt');
+  grid[1][3].setType('belt');
 }
 
 function draw() {
