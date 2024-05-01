5ded7bd3762322f7c046e6a6fc60fea832c12309
KonkenBonken <konrad05pettersson@gmail.com>
2024-05-01 16:37:26 +0200

    setup canvas

---
+++ script.mjs
@@ -0,0 +1,10 @@
+function setup() {
+  createCanvas(
+    min(document.body.clientWidth, document.body.clientHeight),
+    min(document.body.clientWidth, document.body.clientHeight)
+  );
+}
+
+function draw() {
+  background(220);
+}
