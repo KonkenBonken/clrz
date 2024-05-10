46f3903c9327255a4fd0c34eee125823e294e444
KonkenBonken <konrad05pettersson@gmail.com>
2024-05-10 18:45:02 +0200

    add second level

--- Levels.mjs
+++ Levels.mjs
@@ -7,4 +7,14 @@ const Levels = [
     },
     goal: 20
   },
+  {
+    setup() {
+      Generator.build(2, 8, 0, [255, 0, 0]);
+      Belt.build(2, 7, 0);
+      Generator.build(7, 2, 2, [0, 255, 0]);
+      Belt.build(7, 3, 3);
+      Goal.build(5, 5, [255 / 2, 255 / 2, 0]);
+    },
+    goal: 10
+  },
 ];
