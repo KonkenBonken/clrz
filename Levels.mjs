d8a2b0515e38cabb606f2d8a7eb0c36d6b1ccbe2
KonkenBonken <konrad05pettersson@gmail.com>
2024-05-10 19:28:12 +0200

    add level

--- Levels.mjs
+++ Levels.mjs
@@ -13,7 +13,16 @@ const Levels = [
       Belt.build(2, 7, 0);
       Generator.build(7, 2, 2, [0, 255, 0]);
       Belt.build(7, 3, 3);
-      Goal.build(5, 5, [255 / 2, 255 / 2, 0]);
+      Goal.build(5, 5, [127.5, 127.5, 0]);
+    },
+    goal: 10
+  },
+  {
+    setup() {
+      Generator.build(0, 0, 1, [255, 0, 0]);
+      Generator.build(0, 1, 1, [255, 255, 255]);
+      Generator.build(0, 2, 1, [255, 255, 255]);
+      Goal.build(9, 9, [255, 191.25, 191.25]);
     },
     goal: 10
   },
